ee9b0350a36792a7a96da09a43dcea56c8b4b60c
Rokr0k <yangeh2225@gmail.com>
2021-09-27 02:04:45 +0900

    fix animation items center

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, protocol } from 'electron';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -12,8 +12,9 @@ let mainWindow;
 const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
+    title: "Team Up",
   });
 
   // and load the index.html of the app.
@@ -29,6 +30,10 @@ const createWindow = () => {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
+
+  protocol.registerFileProtocol("teamup-dir", (req, callback) => {
+    callback({path: `${__dirname}${new URL(req.url).pathname}`});
+  })
 };
 
 // This method will be called when Electron has finished
