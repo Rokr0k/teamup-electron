e8bfcf6e3d260238de15505e66c35f426d380211
Rokr0k <yangeh2225@gmail.com>
2021-09-27 01:12:36 +0000

    initial commit

---
+++ src/script.js
@@ -0,0 +1,164 @@
+var start = document.getElementById("start");
+const length = document.getElementById("length");
+const except = document.getElementById("except");
+const count = document.getElementById("count");
+const run = document.getElementById("run");
+const save = document.getElementById("save");
+const container = document.getElementById("container");
+const wish = document.getElementById("wish");
+const chang = document.getElementById("chang");
+const animation = document.getElementById("animation");
+const bgs = document.getElementById("bgs");
+
+let teamsObj = null;
+
+run.addEventListener("click", () => {
+    if (animation.checked) {
+        run.disabled = true;
+        wish.removeAttribute("hidden");
+        wish.play();
+    }
+    else {
+        roll().then(teams => work(teams));
+    }
+});
+
+wish.addEventListener("ended", () => {
+    wish.setAttribute("hidden", "");
+    roll().then(teams => {
+        tee = teams;
+        ii = 0;
+        bgs.removeAttribute("hidden");
+        iterate();
+    });
+});
+
+const roll = () => {
+    return new Promise(resolve => {
+        const array = new Array();
+        const exceptions = except.value.split(" ").map(v => +v).filter(v => typeof (v) == "number");
+        for (let i = +start.value; i < +start.value + +length.value; i++) {
+            let done = false;
+            for (let j = 0; j < exceptions.length; j++) {
+                if (i == exceptions[j]) {
+                    done = true;
+                    break;
+                }
+            }
+            if (!done) {
+                array.push(i);
+            }
+        }
+        const teams = new Array(+count.value);
+        for (let i = 0; i < +count.value; i++) {
+            teams[i] = new Array();
+        }
+        let team = 0;
+        while (array.length > 0) {
+            const index = Math.floor(Math.random() * array.length);
+            teams[team].push(array[index]);
+            team = (team + 1) % (+count.value);
+            array.splice(index, 1);
+        }
+        for (let i = 0; i < teams.length; i++) {
+            teams[i] = teams[i].sort((a, b) => a - b);
+        }
+        resolve(teams);
+    })
+}
+
+let tee = null;
+let ii = 0;
+let previousTeam = null;
+let previousIndex = null;
+let previousTimeout = 0;
+
+window.onkeydown = e => {
+    if (bgs.getAttribute("hidden") === null && e.code === "Enter") {
+        ii++;
+        iterate();
+    }
+};
+window.onmousedown = () => {
+    if (bgs.getAttribute("hidden") === null) {
+        ii++;
+        iterate();
+    }
+};
+
+const iterate = () => {
+    if (previousTeam) {
+        previousTeam.remove();
+    }
+    if (previousIndex) {
+        previousIndex.remove();
+    }
+    if (previousTimeout) {
+        clearTimeout(previousTimeout);
+    }
+    if (ii < tee.length) {
+        previousTeam = document.createElement("div");
+        previousTeam.setAttribute("class", "iter");
+        previousTeam.setAttribute("style", "animation-name: show; animation-duration: 1.4s;");
+        for (let i = 0; i < tee[ii].length; i++) {
+            var child = document.createElement("p");
+            child.innerHTML = tee[ii][i];
+            previousTeam.appendChild(child);
+        }
+        bgs.appendChild(previousTeam);
+        previousIndex = document.createElement("div");
+        previousIndex.setAttribute("class", "indx");
+        previousIndex.setAttribute("style", "animation-name: appear; animation-duration: 0.28s; animation-delay: 1.12s; opacity: 0%;");
+        previousIndex.innerHTML = ii + 1;
+        bgs.appendChild(previousIndex);
+        previousTimeout = setTimeout(e => {
+            e.style.opacity = "100%";
+            previousTimeout = 0;
+        }, 1260, previousIndex);
+        chang.currentTime = 0;
+        chang.play();
+    }
+    else {
+        bgs.setAttribute("hidden", "");
+        work(tee);
+        run.disabled = false;
+    }
+}
+
+const work = (teams) => {
+    container.innerHTML = "";
+    for (let i = 0; i < teams.length; i++) {
+        const element = document.createElement("div");
+        element.setAttribute("class", "item");
+        if (animation.checked) {
+            element.setAttribute("style", "animation-name: appear; animation-duration: 1s; animation-delay: " + i * 0.1 + "s; opacity: 0%;");
+        }
+        for (let j = 0; j < teams[i].length; j++) {
+            var child = document.createElement("p");
+            child.innerHTML = teams[i][j];
+            element.appendChild(child);
+        }
+        if (animation.checked) {
+            setTimeout(e => {
+                e.style.opacity = "100%";
+            }, i * 100 + 500, element);
+        }
+        container.appendChild(element);
+    }
+    if (animation.checked) {
+        chang.play();
+    }
+    teamsObj = teams;
+}
+
+save.onclick = () => {
+    if (teamsObj) {
+        const keys = Object.keys(teamsObj);
+        const content = [...teamsObj.map(row => keys.map(i => JSON.stringify(row[i], (_key, value) => value == undefined ? '' : value)).join(','))].join('\r\n');
+        const blob = new Blob([content], { type: 'text/csv' });
+        const anchor = document.createElement("a");
+        anchor.download = "teams.csv";
+        anchor.href = URL.createObjectURL(blob);
+        anchor.click();
+    }
+}
